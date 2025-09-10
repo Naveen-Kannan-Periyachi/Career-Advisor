cf41bbfee0ad360539cb9de41c95da58b97b4140
Naveen-Kannan-Periyachi <naveen2005kannan20@gmail.com>
2025-09-10 07:18:49 +0530

    Initial commit

--- complete/V1/career-advisor-platform/backend/models/Recommendation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const recommendationSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  recommendations: { type: Array, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Recommendation', recommendationSchema);
